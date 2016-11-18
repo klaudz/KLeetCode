0557aa183fbe96e8d05f0821b3af485945bcf1fe
Klaudz Liang <klaudzliang@gmail.com>
2016-11-18 14:20:44 +0800

    Initial Commit

---
+++ kLeetCode/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  kLeetCode
+//
+//  Created by klaudz on 18/11/2016.
+//  Copyright © 2016 klaudz. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
