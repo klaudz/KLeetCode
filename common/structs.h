7e5f4bee1a4afb25f8393f485efbb61dc9196f0c
klaudz <klaudzliang@gmail.com>
2020-08-24 16:48:30 +0800

    feature(Common): Create the umbrella header of structs

---
+++ common/structs.h
@@ -0,0 +1,14 @@
+//
+//  list_node.h
+//  KLeetCode
+//
+//  Created by klaudz on 11/26/16.
+//  Copyright © 2016 klaudz. All rights reserved.
+//
+
+#ifndef structs_h
+#define structs_h
+
+#include "struct_list_node.h"
+
+#endif /* structs_h */
