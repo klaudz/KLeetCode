c4c7f3e378188122a846f4606ad4db73d8a04035
klaudz <klaudzliang@gmail.com>
2020-08-26 17:14:32 +0800

    feature(DoublyLinkedList): Add methods for operating nodes

--- common_ruby/doubly_linked_list.rb
+++ common_ruby/doubly_linked_list.rb
@@ -31,4 +31,39 @@ class DoublyLinkedList
         list = self.new(head, tail)
         list
     end
+    def remove(node)
+        @head = node.next if @head == node
+        @tail = node.prev if @tail == node
+        if node.equal?(@tail)
+            @tail = node.prev
+            puts "x"
+        end
+        node.prev.next = node.next if node.prev
+        node.next.prev = node.prev if node.next
+        node.prev = nil
+        node.next = nil
+    end
+    def unshift(node)
+        node.prev = nil
+        node.next = @head
+        @head.prev = node if @head
+        @head = node
+        @tail = node if @tail.nil?
+    end
+    def push(node)
+        node.prev = @tail
+        node.next = nil
+        @tail.next = node if @tail
+        @head = node if @head.nil?
+        @tail = node
+    end
+    def description
+        infos = []
+        node = @head
+        while node
+            infos.push(node.value)
+            node = node.next
+        end
+        "[ #{infos.join(", ")} ]"
+    end
 end
